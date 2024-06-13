6679b2cc64026afc00a28c6782c9012d4c28401d
sherrenjie <jiesherren@gmail.com>
2024-06-13 18:31:57 -0400

    needleman algorithm

---
+++ alignment.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def needleman_wunsch(seq1, seq2, match=1, mismatch=-1, gap=-1):
+    n = len(seq1)
+    m = len(seq2)
+    score = np.zeros((n+1, m+1))
+
+    for i in range(n+1):
+        score[i][0] = gap * i
+    for j in range(m+1):
+        score[0][j] = gap * j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            match_score = score[i-1][j-1] + (match if seq1[i-1] == seq2[j-1] else mismatch)
+            delete = score[i-1][j] + gap
+            insert = score[i][j-1] + gap
+            score[i][j] = max(match_score, delete, insert)
+
+    align1, align2 = '', ''
+    i, j = n, m
+    while i > 0 and j > 0:
+        current_score = score[i][j]
+        diagonal_score = score[i-1][j-1]
+        up_score = score[i-1][j]
+        left_score = score[i][j-1]
+
+        if current_score == diagonal_score + (match if seq1[i-1] == seq2[j-1] else mismatch):
+            align1 += seq1[i-1]
+            align2 += seq2[j-1]
+            i -= 1
+            j -= 1
+        elif current_score == up_score + gap:
+            align1 += seq1[i-1]
+            align2 += '-'
+            i -= 1
+        else:
+            align1 += '-'
+            align2 += seq2[j-1]
+            j -= 1
+
+    while i > 0:
+        align1 += seq1[i-1]
+        align2 += '-'
+        i -= 1
+    while j > 0:
+        align1 += '-'
+        align2 += seq2[j-1]
+        j -= 1
+
+    return align1[::-1], align2[::-1], score[n][m]
